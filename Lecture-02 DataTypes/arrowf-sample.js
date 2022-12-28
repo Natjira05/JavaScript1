d3c8b1d61c609e6bd08ba712141738576e5109db
Natjira05 <s6406021621099@email.kmutnb.ac.th>
2022-12-28 23:02:18 +0700

    ok

--- Lecture-02 DataTypes/arrowf-sample.js
+++ Lecture-02 DataTypes/arrowf-sample.js
@@ -2,3 +2,20 @@
 const sum = (firstParam, secondParam) => {
   return firstParam + secondParam ;
 };
+console.log(sun(2,5)) ; // Prints: 7
+
+//Arrow function with no arguments
+const printHello = () => {
+  console.log("hello");
+}
+printHello(); // Prints: hello
+
+// Arrow functions with a single argument
+const checkWeight = (weight) => {
+  console.log('Baggage weight : ${weight} kilograms.');
+}
+checkWeight(25); // Prints: Baggage weight : 25 kilograms.
+
+//Concise arrow function
+const multiply = (a,b) => a*b ;
+console.log(multiply(2,30)); // Prints: 60
