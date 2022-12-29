e9d5cc74b85d95f438c8e55ef829c19f00b8c2ad
Natjira05 <s6406021621099@email.kmutnb.ac.th>
2022-12-29 09:34:43 +0700

    ca

---
+++ Lecture-02 DataTypes/example1.js
@@ -0,0 +1,10 @@
+function findPerimeter(length , width) 
+{
+   let findPerimeter = (length * 2) + (width * 2) ;
+   return findPerimeter ;
+}
+
+console.log(findPerimeter(6,7));
+console.log(findPerimeter(20,10));
+console.log(findPerimeter(2,9));
+
