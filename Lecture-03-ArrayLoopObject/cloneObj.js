d602f6b973f818e125bfd5533740d5e22694fa62
Natjira05 <s6406021621099@email.kmutnb.ac.th>
2023-01-04 15:09:48 +0700

    lecture3

---
+++ Lecture-03-ArrayLoopObject/cloneObj.js
@@ -0,0 +1,16 @@
+// Object clonning
+// Obj Ref, Obj Clonning assign(), {....x}, JSON
+//
+
+const obj1 = {
+  person : 'Anirach',
+  weight : 85 
+}
+
+// const obj2 = obj1 ;
+// const obj2 = object.assign({}, obj1);
+
+const obj2 = {...obj1};
+obj2.weight = 75 ;
+console.log(obj1);
+console.log(obj2);
